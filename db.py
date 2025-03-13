d3b2c5757095ec18537f6d6a95d2fc5b06f596db
Ollkyl <uos1999@gmail.com>
2025-03-13 14:50:13 +0400

    2 laptop

---
+++ db.py
@@ -0,0 +1,149 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    BigInteger,
+    DateTime,
+    ARRAY,
+    func,
+    update,
+    delete,
+    select,
+    or_
+)
+from dotenv import dotenv_values
+
+env_values = dotenv_values(".env")
+
+DB_USER = env_values.get("DB_USER")
+DB_PASS = env_values.get("DB_PASS")
+DB_HOST = env_values.get("DB_HOST", "localhost")
+DB_PORT = env_values.get("DB_PORT", "5432")
+DB_NAME = env_values.get("DB_NAME")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    user_id = Column(BigInteger, nullable=True)
+    min_price = Column(Integer, nullable=True)
+    max_price = Column(Integer, nullable=True)
+    rooms = Column(String(500), nullable=True)
+    district = Column(String(1400), nullable=True)
+    period = Column(String(50), nullable=True)
+    status = Column(String(50), nullable=True)
+    user_name = Column(String(255), nullable=True)
+
+
+class Apartment(Base):
+    __tablename__ = "apartments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    owner = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    rooms = Column(String)
+    district = Column(String(1200), nullable=True)
+    period = Column(String, nullable=True)
+    info = Column(String, nullable=True)
+    created_at = Column(DateTime, server_default=func.now())
+    photo_id = Column(ARRAY(String))
+
+
+async def add_client(user_id, min_price, max_price, rooms, district, period, user_name):
+    async with async_session() as session:
+        async with session.begin():
+            stmt_update = (
+                update(Client).where(Client.user_id == user_id).values(status="N")
+            )
+            await session.execute(stmt_update)
+
+            await session.flush()
+            if user_name == None:
+                user_name = "username"
+            stmt_delete = delete(Client).where(Client.status == "N")
+            await session.execute(stmt_delete)
+            new_client = Client(
+                user_id=user_id,
+                min_price=min_price,
+                max_price=max_price,
+                rooms=rooms,
+                district=district,
+                period=period,
+                user_name=user_name,
+                status="Y",
+            )
+            session.add(new_client)
+        await session.commit()
+
+
+async def add_apartment(owner, name, price, rooms, district, period, info, photo_id):
+    """Добавление квартиры и уведомление подходящих клиентов."""
+    async with async_session() as session:
+        async with session.begin():
+            new_apartment = Apartment(
+                owner=owner,
+                name=name,
+                price=price,
+                period=period,
+                rooms=rooms,
+                district=district,
+                info=info,
+                photo_id=photo_id,
+            )
+            session.add(new_apartment)
+            await session.flush()
+
+            matching_clients = await find_matching_clients(new_apartment)
+
+        await session.commit()
+        return new_apartment.id, matching_clients
+
+
+async def find_matching_clients(apartment):
+    """Поиск клиентов, чьи параметры совпадают с новой квартирой."""
+    async with async_session() as session:
+        query = select(Client) 
+        # Условия фильтрации
+        if apartment.price:
+            query = query.where(
+                (Client.min_price <= apartment.price)
+                & (Client.max_price >= apartment.price)
+            )
+
+        if apartment.rooms:
+            query = query.where(
+                (Client.rooms.contains(apartment.rooms)) | (Client.rooms == "Не выбрано")
+            )
+        if apartment.district:
+            query = query.where(
+                (Client.district.contains(apartment.district)) | (Client.district == "Не выбрано")
+            )
+        if apartment.period:
+            query = query.where(
+                (Client.period.contains(apartment.period)) | (Client.period == "Не выбрано")
+            )
+        result = await session.execute(query)
+        clients = result.scalars().all()
+        print(f"Найденные клиенты: {clients}")
+        return [(client.user_id, client.user_name) for client in clients]
+    
+async def get_all_users():
+    async with async_session() as session:
+     
+        result = await session.execute(select(Client.user_id)) 
+        users = result.scalars().all()
+    return users
--- tg_auto3.py
+++ tg_auto3.py
@@ -110,10 +110,8 @@ async def send_latest_posts():
         "session_name", api_id, api_hash, system_version="4.16.30-vxCUSTOM"
     ) as client:
         while True:
-            saved_messages = await client.get_messages(
-                "me", limit=20
-            )  # Берём 20 последних сообщений
-
+            saved_messages = await client.get_messages("me", limit=100)
+           
             if not saved_messages:
                 print("⚠ Нет новых сообщений в сохраненных.")
                 await asyncio.sleep(30)  # Ждём 30 сек перед следующей проверкой
@@ -124,22 +122,27 @@ async def send_latest_posts():
 
             # Группируем сообщения
             for msg in saved_messages:
+                print(f"Сообщение {msg.id}, grouped_id: {msg.grouped_id}, имеет медиа: {bool(msg.media)}")
                 if msg.grouped_id:
                     albums.setdefault(msg.grouped_id, []).append(msg)
                 else:
                     individual_messages.append(msg)  # Это текстовое сообщение без медиа
 
                 # Пересылаем альбомы
+            print(f"⚡ Группировка завершена. Всего альбомов: {len(albums)}, сообщений без медиа: {len(individual_messages)}")
 
             async def album_task():
+                if not albums:  # Если альбомы пусты
+                    print("⚠ Нет альбомов для пересылки.")
                 for album in albums.values():
+                    print(f"⚡ Отправка альбома с {len(album)} сообщениями.")
                     await forward_album(client, channels_with_photos, album)
                     await asyncio.sleep(4400)
 
-                # Пересылаем обычные сообщения
 
             async def msg_task():
                 for msg in individual_messages:
+                    print(f"⚡ Отправка сообщения: {msg.id}")
                     await forward_to_channels(client, channels_without_photos, msg)
                     await asyncio.sleep(4400)
 
@@ -155,7 +158,9 @@ async def forward_album(client, channels, album):
     text = next((msg.message for msg in reversed(album) if msg.message), "")
 
     media_group = [msg for msg in album]  # Все вложения из альбома
-
+    if not media_group:
+        print("⚠ Нет медиа в альбоме, пропускаем.")
+        return
     for channel in channels:
         try:
             await client.send_file(
@@ -166,10 +171,10 @@ async def forward_album(client, channels, album):
             await asyncio.sleep(3)
             print(f"✅ Альбом переслан в {channel}")
         except ChatWriteForbiddenError:
-            print(f"❌ Нет прав на отправку в {channel}")
+            print(f"❌ Нет прав на отправку альбома в {channel}")
             await asyncio.sleep(3)
         except Exception as e:
-            print(f"❌ Ошибка пересылки в {channel}: {e}")
+            print(f"❌ Ошибка пересылки альбома в {channel}: {e}")
             await asyncio.sleep(3)
     print(" forward_albumtttttttttttttttttt")
     await asyncio.sleep(30)
@@ -184,11 +189,11 @@ async def forward_to_channels(client, channels, message):
             print(f"✅ Сообщение переслано в {channel}")
 
         except ChatWriteForbiddenError:
-            print(f"❌ Нет прав на отправку в {channel}")
+            print(f"❌ Нет прав на отправку сообщения в {channel}")
             await asyncio.sleep(3)
         except Exception as e:
             await asyncio.sleep(3)
-            print(f"❌ Ошибка пересылки в {channel}: {e}")
+            print(f"❌ Ошибка пересылки сообщения в {channel}: {e}")
             await asyncio.sleep(3)
     print(" forward_to_channelstttttttttttttttttt")
     await asyncio.sleep(30)
