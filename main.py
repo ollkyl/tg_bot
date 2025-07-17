84c8c53b64029077be041aed6a5aebd7ab6635a0
ollkyl <uos1999@gmail.com>
2025-07-18 00:35:43 +0400

    docker

--- main.py
+++ main.py
@@ -5,16 +5,38 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from bot.handlers import register_handlers
 from parser.parser import main_parser
 from dotenv import dotenv_values
+import asyncpg
 
-logging.basicConfig(level=logging.INFO, filename="app.log")
-logger = logging.getLogger(__name__)
-
+# Загрузка переменных из .env
 env_values = dotenv_values(".env")
 API_TOKEN = env_values["API_TOKEN"]
 ADMIN_ID = int(env_values["ADMIN_ID"])
 
+DB_HOST = env_values["DB_HOST"]
+DB_PORT = env_values["DB_PORT"]
+DB_NAME = env_values["DB_NAME"]
+DB_USER = env_values["DB_USER"]
+DB_PASS = env_values["DB_PASS"]
 
+
+# Ожидание запуска PostgreSQL
+async def wait_for_postgres():
+    while True:
+        try:
+            conn = await asyncpg.connect(
+                user=DB_USER, password=DB_PASS, database=DB_NAME, host=DB_HOST, port=int(DB_PORT)
+            )
+            await conn.close()
+            print("✅ PostgreSQL доступен!")
+            return
+        except Exception as e:
+            print(f"⏳ Ожидание PostgreSQL: {e}")
+            await asyncio.sleep(2)
+
+
+# Основной запуск бота
 async def main():
+    await wait_for_postgres()
     bot = Bot(token=API_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     register_handlers(dp, bot, ADMIN_ID)
