fabc0efef862c3e9a97e0ddea3f0d89a6828dcb3
ollkyl <uos1999@gmail.com>
2025-07-18 14:27:49 +0400

    render.yaml

--- db.py
+++ db.py
@@ -19,14 +19,18 @@ import os
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
 
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# Получаем DATABASE_URL из переменной окружения (на Render она есть)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-print(f"Connecting to database at {DATABASE_URL}")
+# Если на локальной машине переменной DATABASE_URL нет — собрать вручную
+if not DATABASE_URL:
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
